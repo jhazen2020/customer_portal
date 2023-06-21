679b5b504265fd9b3560011a4015b8ad9069a80a
Jesse Hazen <jhazen2020@gmail.com>
2023-06-21 15:58:48 -0600

    add white to navbar mobile menu. add name for title.

--- src/components/navigation/mobile/mobile-nav-bar-brand.js
+++ src/components/navigation/mobile/mobile-nav-bar-brand.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const MobileNavBarBrand = ({ handleClick }) => {
   return (
     <div onClick={handleClick} className="mobile-nav-bar__brand">
-      <NavLink to="/">
-        <img
-          className="mobile-nav-bar__logo"
-          src="https://djkmbr0egwze9.cloudfront.net/jesse_hazen_long.svg"
-          alt="Auth0 shield logo"
-          width="82"
-          height="24"
-        />
-      </NavLink>
+      <Link to="/">
+      Jesse Hazen
+      </Link>
     </div>
   );
 };
