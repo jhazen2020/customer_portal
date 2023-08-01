245a00d219d720fa06bf2abeb8281d71986002f1
Jesse Hazen <jhazen2020@gmail.com>
2023-08-01 10:15:00 -0600

    chore: added a logo

--- src/components/navigation/desktop/nav-bar-brand.js
+++ src/components/navigation/desktop/nav-bar-brand.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+const logo = "https://djkmbr0egwze9.cloudfront.net/JH.svg";
 export const NavBarBrand = () => {
   return (
     <div className="nav-bar__brand">
       <Link to="/">
-      Jesse Hazen
+      <img className="nav-bar__logo" src={logo} alt="React logo" />
       </Link>
     </div>
   );
